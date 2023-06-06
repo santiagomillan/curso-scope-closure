2e432f13bef6e8c4e02828b67ec889cd1244de95
Santiago Millan <104397283+SantiagoNivelics@users.noreply.github.com>
2023-06-06 13:03:17 -0500

    clase 5

---
+++ src/scope/re.js
@@ -0,0 +1,24 @@
+// Reasignación y redeclaración
+
+var fN
+fN = "Oscar"
+console.log(fN)
+
+var lN = "David"
+lN = "Ana"
+console.log(lN)
+
+let fr = "apple"
+fr = "Kiwi"
+console.log(fr)
+//no se puede re declarar let fr = "apple2"
+
+const animal ="dog"
+console.log(animal)
+//no se puede cambiar el valor
+
+const vehicles = []
+vehicles.push("car")
+console.log(vehicles)
+vehicles.pop()
+console.log(vehicles)
